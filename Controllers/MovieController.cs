09acb0e6e545032a48ad77b24446355613f6e9dd
raulntjj <rauloliveira5230@gmail.com>
2025-09-03 09:38:39 -0300

    first commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using movies_api.Models;
+
+namespace movies_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+  	private static List<Movie> movies = new List<Movie>();
+
+	[HttpGet]
+	public List<Movie> getMovies()
+	{
+		return movies;
+	}
+
+	[HttpPost]
+	public void AddMovie([FromBody] Movie movie)
+	{
+		movies.Add(movie);
+		Console.WriteLine(movie.Title);
+	}
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+dotnet new webapi --name movies_api
+dotnet run --project movies_api/movies_api.csproj
